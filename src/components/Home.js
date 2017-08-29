50e5d1e204f965cb2b5e571b6b577be28c491aad
Dave Guymon <davidwguymon@gmail.com>
2017-08-29 09:58:54 -0600

    Completed desktop banner for home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,36 @@
 import React from 'react';
 import './../styles/mainContent.css';
+import './../styles/home.css';
 
 export default function Home (props) {
   return (
     <main className="mainContent">
-      <h1>This is the Home Component</h1>
+      <section className="homeBanner">
+        <div className="quoteContainer">
+          <div className="socialIconsQuote">
+            <a href="https://www.facebook.com/ElSistemaPittsburgh/" target="_blank">
+              <div className="socialCircleQuote"><i className="fa fa-facebook fa-fw" aria-hidden="true"></i></div>
+            </a>
+
+            <a href="https://twitter.com/elsistema" target="blank">
+              <div className="socialCircleQuote"><i className="fa fa-twitter fa-fw" aria-hidden="true"></i></div>
+            </a>
+
+            <a href="https://www.linkedin.com/company/3276954/" target="_blank">
+              <div className="socialCircleQuote"><i className="fa fa-linkedin fa-fw" aria-hidden="true"></i></div>
+            </a>
+          </div>
+
+          <blockquote>
+            <p className="quote">"From the minute a child is taught to play an instrument, he's no longer poor. He is a child in progress who will become a citizen."</p>
+            <div className="quoteAttribute">
+              <p className="author">Dr. Jose Antonio Abreu</p>
+              <p className="authorTitle">Founder of {"Venezuela's"} El Sistema</p>
+            </div>
+          </blockquote>
+
+        </div>
+      </section>
     </main>
   )
 }
