3cfff6d39199f7ee50866bd56108e3e24a27bb62
Dave Guymon <davidwguymon@gmail.com>
2017-08-28 23:01:37 -0600

    Completed footer style for tablet view.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,30 +6,56 @@ export default function Footer(props){
     <footer className="footer">
 
       <div className="topFooterDiv">
-        <section className="stayConnected">
+        <section className="stayConnectediPhone">
           <p>STAY CONNECTED</p>
           <div className="socialIcons">
+            <a href="https://www.facebook.com/ElSistemaPittsburgh/" target="_blank">
+              <div className="socialCircle"><i className="fa fa-facebook fa-fw" aria-hidden="true"></i></div>
+            </a>
 
-          <a href="https://www.facebook.com/ElSistemaPittsburgh/" target="_blank">
-            <div className="socialCircle"><i className="fa fa-facebook fa-fw" aria-hidden="true"></i></div>
-          </a>
+            <a href="https://twitter.com/elsistema" target="blank">
+              <div className="socialCircle"><i className="fa fa-twitter fa-fw" aria-hidden="true"></i></div>
+            </a>
 
-          <a href="https://twitter.com/elsistema" target="blank">
-            <div className="socialCircle"><i className="fa fa-twitter fa-fw" aria-hidden="true"></i></div>
-          </a>
-
-          <a href="https://www.linkedin.com/company/3276954/" target="_blank">
-            <div className="socialCircle"><i className="fa fa-linkedin fa-fw" aria-hidden="true"></i></div>
-          </a>
-          
+            <a href="https://www.linkedin.com/company/3276954/" target="_blank">
+              <div className="socialCircle"><i className="fa fa-linkedin fa-fw" aria-hidden="true"></i></div>
+            </a>
           </div>
         </section>
 
+        <section className="stayConnectedTabDesk">
+          <h3>STAY CONNECTED</h3>
+          <p>Enter your mobile number to start receiving text message notifications from El Sistema Pittsburgh! Unsubscribe at any time.</p>
+          <div className="signUpForTexts">
+          <input className="textInput"/>
+          <div className="signUpButton">
+            <p>Sign-Up</p>
+          </div>
+          </div>
+          <div className="socialIcons">
+            <a href="https://www.facebook.com/ElSistemaPittsburgh/" target="_blank">
+              <div className="socialCircle"><i className="fa fa-facebook fa-fw" aria-hidden="true"></i></div>
+            </a>
+
+            <a href="https://twitter.com/elsistema" target="blank">
+              <div className="socialCircle"><i className="fa fa-twitter fa-fw" aria-hidden="true"></i></div>
+            </a>
+
+            <a href="https://www.linkedin.com/company/3276954/" target="_blank">
+              <div className="socialCircle"><i className="fa fa-linkedin fa-fw" aria-hidden="true"></i></div>
+            </a>
+          </div>
+        </section>
+
+        <section className="committedToPitt">
+          <h3>COMMITTED TO PITTSBURGH</h3>
+          <p>El Sistema Pittsburgh has successfully developed a diverse, qualified, and competent leadership committee focused on ensuring artistic and musical education. In part, a highly unique curriculum has been developed to meet the distinct needs of community residents while alleviating the current budget void that has transversely impacted public {"schools'"} art and music programs.</p>
+        </section>
       </div>
 
       <div className="bottomFooterDiv">
         <p>El Sistema Pittsburgh - 6 Loop St. #4, Pittsburgh, PA 15215 - Map - Main (777) 465-2342</p>
-        <p>Privacy Policy - Site Map - &copy Copyright 2017</p>
+        <p>Privacy Policy - Site Map - &copy; Copyright 2017</p>
       </div>
     </footer>
   )
