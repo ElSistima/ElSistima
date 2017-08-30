8fd4aa045a9d8f51b343555293a5ef9b5403befd
yzhou2 <samandjesslovepie@gmail.com>
2017-08-30 09:24:39 -0600

    Admin portal 90% done

--- src/components/AdminPortal.js
+++ src/components/AdminPortal.js
@@ -1,15 +1,44 @@
 import React, {Component} from 'react';
+import {Route, Switch} from 'react-router-dom';
+import AdminHeader from './AdminHeader';
+import AdminSideBar from './AdminSideBar';
+import AdminHome from './AdminHome';
+import AdminVolunteer from './AdminVolunteer';
 
 export default class AdminPortal extends Component{
   constructor(props){
     super(props);
 
+    this.state ={
+      sidebarOn:true
+    }
+  this.hamClicked = this.hamClicked.bind(this)
   }
 
+  hamClicked(){
+    this.setState({
+      sidebarOn: !this.state.sidebarOn
+    })
+  }
+
+
   render(){
+    var hiddenHam = {
+      display: "none"
+    }
+    console.log(this.state.sidebarOn)
     return(
       <main>
+        <AdminHeader hamClicked={this.hamClicked}/>
+        <div style={this.state.sidebarOn?{}:hiddenHam}>
+          <AdminSideBar />
+        </div>
 
+
+        <Switch>
+          <Route exact path="/admin" component={AdminHome} />
+          <Route exact path="/admin/volunteer" component={AdminVolunteer} />
+        </Switch>
       </main>
     )
   }
---
+++ src/components/AdminVolunteer.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+import '../styles/adminVolunteer.css';
+
+
+export default class AdminHome extends Component{
+  constructor(props){
+    super(props);
+
+  }
+
+  render(){
+    return(
+      <main className="Ad_VTMain">
+      </main>
+    )
+  }
+}
