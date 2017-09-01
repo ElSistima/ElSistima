d1715db5af5569c4a462663b82e559a30455a6ae
yzhou2 <samandjesslovepie@gmail.com>
2017-09-01 12:28:19 -0600

    Support us page - payment

--- src/components/Support.js
+++ src/components/Support.js
@@ -8,11 +8,40 @@ export default function Support(props){
         <div className="supportTitle">Support El Sistema Pittsburgh</div>
         <div className="supportInnerBox">
           <div className="innerBoxTitle">Our Goals</div>
-          <div className="innerBody">El Sistema Pittsburgh Provides Pittsburghs youth with accessible</div>
+          <div className="innerBody">El Sistema Pittsburgh Provides Pittsburghs youth with accessible and intensive classical music instruction. Utilizing music as a vehicle for positive change, ESPGH promotes the development of music, cognitive and social skills, self-esteem, and community pride in youth.</div>
         </div>
         <div className="supportInnerBox">
           <div className="innerBoxTitle">How Donations are Used</div>
-          <div className="innerBody"></div>
+          <div className="innerBody">100% of all Donations are used to help fund El Sistemas materials, teachers, and concerts. Paypal takes a slight cut of 7% but the rest is distributed based on what is needed at the time. El Sistema complies with all E32c regulations and takes great pride in its transparency.</div>
+        </div>
+      </div>
+
+      <div className="waysToHelp">
+        <div className="helpTitle">3 Ways to Help</div>
+        <div className="donate">
+          <div className="donateTitle">Donate</div>
+          <div className="donateInnerBox">
+            <input className="enterAmount" />
+            <img src='https://i.imgur.com/hUaDjhR.png' />
+          </div>
+          <div className="donateInner">
+            <div className="donateInnerTitle">Credit Card Number</div>
+            <input className="cardNumInput" />
+          </div>
+          <div className="donateInner">
+            <div className="donateInnerTitle">Expiration</div>
+            <input />
+          </div>
+          <div className="donateInner">
+            <div className="donateInnerTitle">CVC/CVV</div>
+            <div className="cvcInputLine">
+              <input className="cvcTitle"/>
+              <div> 3 or 4 digit code</div>
+            </div>  
+          </div>
+          <div className="donateInner">
+            <div className="submitBtn"></div>
+          </div>
         </div>
       </div>
     </main>
