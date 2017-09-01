406edb0a77d6a29c4e2dfd1e7ebb32398bc059dc
Dave Guymon <davidwguymon@gmail.com>
2017-09-01 11:10:50 -0600

    Committing changes prior to pulling from master

--- src/components/AdminBlog_ADDNEW.js
+++ src/components/AdminBlog_ADDNEW.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import '../styles/adminBlogEditor.css';
+import '../styles/adminBlogNew.css';
 import {connect} from 'react-redux';
 
 class AdminBlog_ADDNEW extends Component{
@@ -13,40 +13,39 @@ class AdminBlog_ADDNEW extends Component{
     const fullPageStyle = { width: "100%" }
 
     return(
-      <main className="AdminBlog_ADDNEW_Main" style={ this.props.dropdownDisplayed ? null : fullPageStyle}>
-      <div className="add_new_blog">
-        <div className="anb_headerText">Add New Blog</div>
-        <div className="anb_topInput">
-          <input placeholder="Title"/>
-          <input className="captionInput" placeholder="Caption"/>
+      <main className="adminWrapperBlogNew" style={ this.props.dropdownDisplayed ? null : fullPageStyle}>
+        <div className="adminContentContainerBlogNew">
+          <div className="addNewBlogNew">
+            <p className="adminPageTitleBlogNew">Update BlogNew</p>
+            <div className="topInputBlogNew">
+              <input placeholder="Title" className="titleBlogNew"/>
+              <input className="captionInputBlogNew" placeholder="Subtitle" className="subtitleBlogNew"/>
+            </div>
+
+            <div className="overwriteBlogNew">
+              <textarea placeholder="Blog content here" className="contentBlogNew"></textarea>
+            </div>
+          </div>
+
+          <div className="addNewPicsBlogNew">
+            <div className="addPicInnerBlogNew">
+              <p className="picInnerTextBlogNew">Add Top Full Picture</p>
+              <img src='https://i.imgur.com/FTLTf6u.png' />
+              <div className="buttonBlogNew updateBtnBlogNew">UPDATE</div>
+            </div>
+            <div className="addPicInnerBlogNew">
+              <p className="picInnerTextBlogNew">Add 2nd Full Picture</p>
+              <img src='https://i.imgur.com/FTLTf6u.png' />
+              <div className="buttonBlogNew cancelBtnBlogNew">CANCEL</div>
+            </div>
+          </div>
+
         </div>
 
-        <div className="maintxt_Content anb_overwrite">
-          <textarea placeholder="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."></textarea>
+        <div className="saveCancelBtnContainerBlogNew">
+          <div className="buttonBlogNew updateBtnBlogNewDesktop">UPDATE</div>
+          <div className="buttonBlogNew cancelBtnBlogNewDesktop">CANCEL</div>
         </div>
-      </div>
-
-
-
-      <div className="add_new_pics">
-        <div className="add_pic_inner">
-          <p className="picInnerText">Add Top Full Picture</p>
-          <img src='https://i.imgur.com/FTLTf6u.png' />
-          <div className="pblg save_btn">SAVE</div>
-        </div>
-        <div className="add_pic_inner">
-          <p className="picInnerText">Add 2nd Full Picture</p>
-          <img src='https://i.imgur.com/FTLTf6u.png' />
-          <div className="pblg cancel_btn">CANCEL</div>
-        </div>
-      </div>
-
-      <div className="web_btn">
-        <div className="singlebtn web_save">SAVE</div>
-        <div className="singlebtn web_cancel">CANCEL</div>
-      </div>
-
-
       </main>
     )
   }
