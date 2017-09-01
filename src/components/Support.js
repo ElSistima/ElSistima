81d39136fad828db341039b411bb78fc80b4c74b
yzhou2 <samandjesslovepie@gmail.com>
2017-09-01 11:11:30 -0600

    working on support

--- src/components/Support.js
+++ src/components/Support.js
@@ -3,8 +3,18 @@ import './../styles/support.css';
 
 export default function Support(props){
   return (
-    <main>
-      <h1>Donate and Volunteer</h1>
+    <main className="supportUsWrapper">
+      <div className="supportTopBody">
+        <div className="supportTitle">Support El Sistema Pittsburgh</div>
+        <div className="supportInnerBox">
+          <div className="innerBoxTitle">Our Goals</div>
+          <div className="innerBody">El Sistema Pittsburgh Provides Pittsburghs youth with accessible</div>
+        </div>
+        <div className="supportInnerBox">
+          <div className="innerBoxTitle">How Donations are Used</div>
+          <div className="innerBody"></div>
+        </div>
+      </div>
     </main>
   )
 }
