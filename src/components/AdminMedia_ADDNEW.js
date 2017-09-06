72b9c464a3f8e62d8fc4f1819edcd5b0aa205ed2
Dave Guymon <davidwguymon@gmail.com>
2017-09-05 20:23:59 -0600

    Completed styling for AdminMedia_AddNew iPhone with S3

--- src/components/AdminMedia_ADDNEW.js
+++ src/components/AdminMedia_ADDNEW.js
@@ -3,6 +3,27 @@ import '../styles/adminMediaNew.css';
 import axios from 'axios';
 import {Link} from 'react-router-dom';
 import {connect} from 'react-redux';
+import Dropzone from 'react-dropzone';
+
+
+const uploadImage = (file) => {
+  return axios.post("http://localhost:80/api/getSignedURL", {
+    filename: file.name,
+    filetype: file.type
+  })
+  .then(res => {
+    let options = {
+      headers: {
+        'Content-Type': file.type
+      }
+    }
+    return axios.put(res.data.url, file, options)
+    .then(res => {
+       return res.config.url.match(/.*\?/)[0].slice(0,-1)
+    })
+  })
+}
+
 
 class AdminMedia_ADDNEW extends Component{
   constructor(props){
@@ -10,13 +31,27 @@ class AdminMedia_ADDNEW extends Component{
       this.state={
         mediaTitle: '',
         mediaCategory: '',
-        mediaDescription: ''
+        mediaDescription: '',
+        picture: 'https://i.imgur.com/FTLTf6u.png'
       }
+  }
+
+  onDrop1(accepted, rejected){
+    uploadImage(accepted[0])
+    .then(url => {
+      this.setState({picture: url})
+  })
   }
 
   render(){
 
     const fullPageStyle = { width: "100%" }
+
+    const placeholder = {
+      backgroundImage: `url('${this.state.picture}')`,
+      backgroundSize: 'cover',
+      backgroundPosition: 'center'
+    }
 
     return(
       <main className="adminWrapperMediaNew" style={ this.props.dropdownDisplayed ? null : fullPageStyle}>
@@ -25,7 +60,13 @@ class AdminMedia_ADDNEW extends Component{
             <p className="adminPageTitleMediaNew">Add New Media</p>
             <div className="topInputMediaNew">
               <input placeholder="Title" value={this.state.mediaTitle} className="titleMediaNew" />
-              <input placeholder="Category" value={this.state.mediaCategory} className="categoryMediaNew" />
+
+              <div className="mediaSelectContainer">
+                <select>
+                  <option value="Image">Image</option>
+                  <option value="Video">Video</option>
+                </select>
+              </div>
             </div>
 
             <div className="midInputMediaNew">
@@ -33,14 +74,17 @@ class AdminMedia_ADDNEW extends Component{
             </div>
 
             <div className="bottomInputMediaNew">
-
+            <Dropzone
+              className="mediaDropzone"
+              style={placeholder}
+              onDrop={(accepted, rejected) => this.onDrop1(accepted, rejected)}></Dropzone>
             </div>
           </div>
         </div>
 
 
         <div className="saveCancelBtnContainerMediaNew">
-          <div className="buttonMediaNew updateBtnMediaNewDesktop">SAVE</div>
+          <div className="updateBtnMediaNewDesktop">SAVE</div>
           <div className="buttonMediaNew cancelBtnMediaNewDesktop">CANCEL</div>
         </div>
       </main>
